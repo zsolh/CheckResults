0744bf849a06158fa4e0297a0ccc212b71235d45
zsolh <holman.zsolt@gmail.com>
2024-07-21 21:11:55 +0200

    Multiple criteria patterns

--- selector.py
+++ selector.py
@@ -7,16 +7,18 @@ def main():
     args = sys.argv[1:]
     directory = 'Log'
     #pattern = '^.*csv$'
-    pattern = args[0]
+    #pattern = args[0]
+    patterns = args
     #pattern = '.*'
-    csv_files = get_files_matching_pattern(directory, pattern)
+    csv_files = get_files_matching_pattern(directory, patterns)
     print("csv files: ")
     print(csv_files)
 
 
-def get_files_matching_pattern(directory, pattern):
+def get_files_matching_pattern(directory, patterns):
     files = [f for f in os.listdir(directory) if os.path.isfile(os.path.join(directory, f))]
-    matching_files = [f for f in files if re.match(pattern, f)]
+    #matching_files = [f for f in files if re.match(pattern, f)]
+    matching_files = [f for f in files if all(re.match(pattern, f) for pattern in patterns)]
     
     return matching_files
 
